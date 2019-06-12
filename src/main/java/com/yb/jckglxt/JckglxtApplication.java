0277d05c6c1e29b0809ffe70e3532e8eaa16081f
NBAISINO\yangbin <1148016766@qq.com>
2019-06-12 11:09:52 +0800

    test

---
+++ src/main/java/com/yb/jckglxt/JckglxtApplication.java
@@ -0,0 +1,13 @@
+package com.yb.jckglxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JckglxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JckglxtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yb/jckglxt/project/controller/ConnectController.java
@@ -0,0 +1,38 @@
+package com.yb.jckglxt.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * ---------------------
+ * Created by Administrator on 2019-06-12.
+ */
+@RestController
+public class ConnectController {
+
+    @RequestMapping("/sss")
+    public void askjdsk(){
+        System.out.println(111111);
+    }
+}
